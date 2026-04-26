1b37108411600356d8b480c01097ed6944d26f85
VeonardoVV <leonardo.vtrno@gmail.com>
2026-04-26 16:39:26 -0500

    fvgbhganaaqaasascqa

--- conexion.js
+++ conexion.js
@@ -2,5 +2,5 @@ import { createClient } from "@supabase/supabase-js";
 
 export const supabase = createClient(
   "https://ghfhxtbjzlyqhzoxioqo.supabase.co",
-  "sb_publishable_qHMWePeCnxoRvPef3lYA2g_xp_Rg9Yz"
+  "sb_publishable_-OfqIsoeaXDHkWlMBHKLrA_k4MWGwrm"
 );
